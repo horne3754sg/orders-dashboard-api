6b368483fd2dd8ec312b74b5c5476916dca2fc5d
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-06-07 13:20:37 +0100

    orders POST route updated

--- models/orders.js
+++ models/orders.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 
 const orderSchema = new mongoose.Schema({
   Product_name: String,
+  Brand: String,
   Category: String,
   Size: String,
   Colour: String,
   Status: String,
-  Customer_Initials: String,
+  Customer_initials: String,
 })
 
 const Order = mongoose.model('Order', orderSchema)
+
+exports.Order = Order
--- routes/orders.js
+++ routes/orders.js
@@ -4,10 +4,36 @@ const express = require('express')
 const router = express.Router()
 
 // Get all orders
-router.get('/', (req, res) => {})
+router.get('/', (req, res) => {
+  res.send([])
+})
 
 // Post new order
-router.post('/', (req, res) => {})
+router.post('/', async (req, res) => {
+  const {
+    Product_name,
+    Brand,
+    Category,
+    Size,
+    Colour,
+    Status,
+    Customer_initials,
+  } = req.body
+
+  const order = new Order({
+    Product_name,
+    Brand,
+    Category,
+    Size,
+    Colour,
+    Status,
+    Customer_initials,
+  })
+
+  await order.save()
+
+  res.send(order)
+})
 
 // Update order
 router.put('/:id', (req, res) => {})
