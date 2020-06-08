40e66c250b98e35497df32ea4fa57f696341d12b
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-06-08 19:20:34 +0100

    error handling middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,9 @@
-const express = require('express')
 const bodyParser = require('body-parser')
 const orders = require('../routes/orders')
+const error = require('../middleware/error')
 
 module.exports = function (app) {
   app.use(bodyParser.json())
   app.use('/api/orders', orders)
+  app.use(error)
 }
